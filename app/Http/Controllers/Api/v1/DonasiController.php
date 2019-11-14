fd084ca8373ec367c52121596942b3098bd04f63
Dedy <dedyjoomla@gmail.com>
2019-11-14 20:38:12 +0700

    donasi berhasil email

--- app/Http/Controllers/Api/v1/DonasiController.php
+++ app/Http/Controllers/Api/v1/DonasiController.php
@@ -1,82 +1,82 @@
 <?php
 
 namespace App\Http\Controllers\Api\v1;
+
 use App\Donasi;
 use Illuminate\Http\Request;
 use Validator;
 use Carbon\Carbon;
 
-
 class DonasiController extends Controller
 {
-  public $path;
-  public $dimensions;
-  public $pathdir;
-  const MODEL = "App\Donasi";
+    public $path;
+    public $dimensions;
+    public $pathdir;
+    const MODEL = "App\Donasi";
 
-  use RESTActions;
+    use RESTActions;
 
     public function create(Request $request)
     {
-      $donasiData = new Donasi();
-      $requestData = $request->all();
+        $donasiData = new Donasi();
+        $requestData = $request->all();
 
-      $validator = Validator::make($requestData,$donasiData->rules);
-      if ($validator->fails()) {
-        return response()->json([
-          $validator->errors()
+        $validator = Validator::make($requestData, $donasiData->rules);
+        if ($validator->fails()) {
+            return response()->json([
+          $validator->errors(),
         ], 417);
-      }
-      $requestData['donation_amount']=(int)$requestData['donation_amount'];
-      $requestData['status']='PENDING';
+        }
+        $requestData['donation_amount'] = (int) $requestData['donation_amount'];
+        $requestData['status'] = 'PENDING';
 
-      return $this->respond('created', Donasi::create($requestData));
+        return $this->respond('created', Donasi::create($requestData));
     }
 
     public function get($id)
     {
-      $model = Donasi::with('kampanye','user')->where('_id',$id)->first();
-      if(is_null($model)){
-        return $this->respond('not_found');
-      }
-      return $this->respond('done', $model);  
+        $model = Donasi::with('kampanye', 'user')->where('_id', $id)->first();
+        if (is_null($model)) {
+            return $this->respond('not_found');
+        }
+
+        return $this->respond('done', $model);
     }
-     
+
     public function bayarDonasi($id)
     {
-      $model = Donasi::find($id);
-      if(is_null($model)){
-        return $this->respond('not_found');
-      }
-      $model->status='PAID';
+        $model = Donasi::find($id);
+        if (is_null($model)) {
+            return $this->respond('not_found');
+        }
+        $model->status = 'PAID';
 
-     $model->kampanye()->increment('donation_collected',$model->donation_amount);
-     $model->kampanye()->increment('total_donatur');
+        $model->kampanye()->increment('donation_collected', $model->donation_amount);
+        $model->kampanye()->increment('total_donatur');
 
-      return $this->respond('done', $model);
+        return $this->respond('done', $model);
     }
 
     public function sendEmailBerhasilDonasi()
     {
-      $dt = Carbon::now()->toFormattedDateString();
-      $domain = env('MAILGUN_DOMAIN');
-      $mailgun_api = env('MAILGUN_API');
+        $dt = Carbon::now()->toFormattedDateString();
+        $domain = env('MAILGUN_DOMAIN');
+        $mailgun_api = env('MAILGUN_API');
 
-      $mgClient = Mailgun::create($mailgun_api);
+        $mgClient = Mailgun::create($mailgun_api);
 
-      $view = view('email.register', ['date' => $dt,
+        $view = view('email.berhasilDonasi', ['campaign' => $dt,
     'name' => 'dedy kurniawan santoso',
-    'token' => $token,
     ])->render();
-      try {
-          $result = $mgClient->sendMessage($domain,
+        try {
+            $result = $mgClient->sendMessage($domain,
           array('from' => 'Donasi GSM <postmaster@'.env('MAILGUN_FROM').'>',
               'to' => $user->email,
-              'subject' => 'Aktivasi Akun Donasi Anda',
+              'subject' => 'Terimakasih! Donasi anda telah diterima',
               'html' => $view, ));
-      } catch (MissingRequiredMIMEParameters $e) {
-      }
+        } catch (MissingRequiredMIMEParameters $e) {
+        }
 
-      return $result;
+        return $result;
     }
 }
